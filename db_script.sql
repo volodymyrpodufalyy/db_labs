4fe9e404a96772e2c64988c85e55651916d70852
Volodymyr Podufalyy <vpodufalyy13@gmail.com>
2021-10-13 20:52:35 +0300

    Update db_script.sql

--- db_script.sql
+++ db_script.sql
@@ -34,6 +34,18 @@ CREATE TABLE IF NOT EXISTS `Podufalyy`.`country` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+INSERT INTO country (id, name, covid_rules) VALUES
+(1, 'Ukraine', 'PLR test and app'),
+(2, 'USA', 'Vaccination sertificate'),
+(3, 'Poland', 'PLR test and vaccination'),
+(4, 'UK', 'Vaccination sertificate'),
+(5, 'Spain', 'Vaccination sertificate'),
+(6, 'Italy', 'Vaccination sertificate and PLR test'),
+(7, 'Germany', 'Vaccination sertificate'),
+(8, 'Belgium', 'Vaccination sertificate'),
+(9, 'Canada', 'PLR test'),
+(10, 'Egypt', 'PLR test');
+
 
 -- -----------------------------------------------------
 -- Table `Podufalyy`.`airline`
@@ -51,6 +63,18 @@ CREATE TABLE IF NOT EXISTS `Podufalyy`.`airline` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO airline (id, name, country_id) VALUES
+(1, 'Ukraine airlines', '1'),
+(2, 'USA airlines', '2'),
+(3, 'Poland airlines', '3'),
+(4, 'UK airlines', '4'),
+(5, 'Spain airlines', '5'),
+(6, 'Italy airlines', '6'),
+(7, 'Germany airlines', '7'),
+(8, 'Belgium airlines', '8'),
+(9, 'Canada airlines', '9'),
+(10, 'Egypt airlines', '10');
+
 
 -- -----------------------------------------------------
 -- Table `Podufalyy`.`city`
@@ -67,6 +91,18 @@ CREATE TABLE IF NOT EXISTS `Podufalyy`.`city` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+INSERT INTO city (id, name, country_id) VALUES
+(1, 'Kyiv', '1'),
+(2, 'New York', '2'),
+(3, 'Warsaw', '3'),
+(4, 'London', '4'),
+(5, 'Madrid', '5'),
+(6, 'Rome', '6'),
+(7, 'Berlin', '7'),
+(8, 'Brussels', '8'),
+(9, 'Toronto', '9'),
+(10, 'Kair', '10');
 
 
 -- -----------------------------------------------------
@@ -86,6 +122,18 @@ CREATE TABLE IF NOT EXISTS `Podufalyy`.`airport` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+INSERT INTO airport (id, name,country_id, city_id) VALUES
+(1, 'Boryspil airport', '1', '1'),
+(2, 'John F. Kennedy International Airport', '2', '2'),
+(3, 'Warsaw Chopin Airport', '3', '3'),
+(4, 'Haethrow airport', '4', '4'),
+(5, 'Madrid-Barajas Adolfo Suárez Airport', '5', '5'),
+(6, 'AEROPORTI DI ROMA', '6', '6'),
+(7, 'Berlin-Tegel airport', '7', '7'),
+(8, 'Brussels Airport', '8', '8'),
+(9, 'Toronto Pearson International Airport', '9', '9'),
+(10, 'Cairo International Airport', '10', '10');
+
 
 -- -----------------------------------------------------
 -- Table `Podufalyy`.`plane`
@@ -104,6 +152,18 @@ CREATE TABLE IF NOT EXISTS `Podufalyy`.`plane` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+INSERT INTO plane (id, name, seats, type, airline_id) VALUES
+(1, 'Boeing 777', '100', 'standart', '1'),
+(2, 'Boeing 772', '100', 'military', '2'),
+(3, 'Boeing 737', '100', 'freight', '3'),
+(4, 'Airbus A320', '100', 'military', '4'),
+(5, 'Airbus A330', '100', 'freight', '5'),
+(6, 'Boeing 787', '100', 'standart', '6'),
+(7, 'BOEING 767-300ER', '100', 'standart', '7'),
+(8, 'BOEING 737-800', '100', 'freight', '8'),
+(9, 'EMBRAER-190', '100', 'standart', '9'),
+(10, 'EMBRAER-195', '100', 'military', '10');
 
 
 -- -----------------------------------------------------
