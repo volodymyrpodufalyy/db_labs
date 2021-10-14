a5e3919be9d1329a33a09f7ea07afc9a6e30198c
Volodymyr Podufalyy <vpodufalyy13@gmail.com>
2021-10-14 17:56:49 +0300

    refactored inserts

--- db_script.sql
+++ db_script.sql
@@ -14,28 +14,28 @@ DROP TABLE IF EXISTS country;
 
 
 CREATE TABLE country (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `covid_rules` VARCHAR(450) NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  covid_rules VARCHAR(450) NULL,
   PRIMARY KEY (`id`));
 
-INSERT INTO country (id, name, covid_rules) VALUES
-(1, 'Ukraine', 'PLR test and app'),
-(2, 'USA', 'Vaccination sertificate'),
-(3, 'Poland', 'PLR test and vaccination'),
-(4, 'UK', 'Vaccination sertificate'),
-(5, 'Spain', 'Vaccination sertificate'),
-(6, 'Italy', 'Vaccination sertificate and PLR test'),
-(7, 'Germany', 'Vaccination sertificate'),
-(8, 'Belgium', 'Vaccination sertificate'),
-(9, 'Canada', 'PLR test'),
-(10, 'Egypt', 'PLR test');
+INSERT INTO country (name, covid_rules) VALUES
+('Ukraine', 'PLR test and app'),
+('USA', 'Vaccination sertificate'),
+('Poland', 'PLR test and vaccination'),
+('UK', 'Vaccination sertificate'),
+('Spain', 'Vaccination sertificate'),
+('Italy', 'Vaccination sertificate and PLR test'),
+('Germany', 'Vaccination sertificate'),
+('Belgium', 'Vaccination sertificate'),
+('Canada', 'PLR test'),
+('Egypt', 'PLR test');
 
 
 CREATE TABLE airline (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `country_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  country_id INT NOT NULL,
   PRIMARY KEY (`id`, `country_id`),
   INDEX `fk_airline_country1_idx` (`country_id` ASC) VISIBLE,
   CONSTRAINT `fk_airline_country1`
@@ -44,49 +44,49 @@ CREATE TABLE airline (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-INSERT INTO airline (id, name, country_id) VALUES
-(1, 'Ukraine airlines', '1'),
-(2, 'USA airlines', '2'),
-(3, 'Poland airlines', '3'),
-(4, 'UK airlines', '4'),
-(5, 'Spain airlines', '5'),
-(6, 'Italy airlines', '6'),
-(7, 'Germany airlines', '7'),
-(8, 'Belgium airlines', '8'),
-(9, 'Canada airlines', '9'),
-(10, 'Egypt airlines', '10');
+INSERT INTO airline (name, country_id) VALUES
+('Ukraine airlines', '1'),
+('USA airlines', '2'),
+('Poland airlines', '3'),
+('UK airlines', '4'),
+('Spain airlines', '5'),
+('Italy airlines', '6'),
+('Germany airlines', '7'),
+('Belgium airlines', '8'),
+('Canada airlines', '9'),
+('Egypt airlines', '10');
 
 
 CREATE TABLE city (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `country_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  country_id INT NOT NULL,
   PRIMARY KEY (`id`, `country_id`),
   INDEX `fk_city_country1_idx` (`country_id` ASC) VISIBLE,
   CONSTRAINT `fk_city_country1`
     FOREIGN KEY (`country_id`)
-    REFERENCES `Podufalyy`.`country` (`id`)
+    REFERENCES country (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-INSERT INTO city (id, name, country_id) VALUES
-(1, 'Kyiv', '1'),
-(2, 'New York', '2'),
-(3, 'Warsaw', '3'),
-(4, 'London', '4'),
-(5, 'Madrid', '5'),
-(6, 'Rome', '6'),
-(7, 'Berlin', '7'),
-(8, 'Brussels', '8'),
-(9, 'Toronto', '9'),
-(10, 'Kair', '10');
+INSERT INTO city (name, country_id) VALUES
+('Kyiv', '1'),
+('New York', '2'),
+('Warsaw', '3'),
+('London', '4'),
+('Madrid', '5'),
+('Rome', '6'),
+('Berlin', '7'),
+('Brussels', '8'),
+('Toronto', '9'),
+('Kair', '10');
 
 
 CREATE TABLE airport (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `country_id` INT NOT NULL,
-  `city_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  country_id INT NOT NULL,
+  city_id INT NOT NULL,
   PRIMARY KEY (`id`, `country_id`, `city_id`),
   INDEX `fk_airport_city1_idx` (`city_id` ASC) VISIBLE,
   CONSTRAINT `fk_airport_city1`
@@ -95,25 +95,25 @@ CREATE TABLE airport (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-INSERT INTO airport (id, name,country_id, city_id) VALUES
-(1, 'Boryspil airport', '1', '1'),
-(2, 'John F. Kennedy International Airport', '2', '2'),
-(3, 'Warsaw Chopin Airport', '3', '3'),
-(4, 'Haethrow airport', '4', '4'),
-(5, 'Madrid-Barajas Adolfo Suárez Airport', '5', '5'),
-(6, 'AEROPORTI DI ROMA', '6', '6'),
-(7, 'Berlin-Tegel airport', '7', '7'),
-(8, 'Brussels Airport', '8', '8'),
-(9, 'Toronto Pearson International Airport', '9', '9'),
-(10, 'Cairo International Airport', '10', '10');
+INSERT INTO airport (name, country_id, city_id) VALUES
+('Boryspil airport', '1', '1'),
+('John F. Kennedy International Airport', '2', '2'),
+('Warsaw Chopin Airport', '3', '3'),
+('Haethrow airport', '4', '4'),
+('Madrid-Barajas Adolfo Suárez Airport', '5', '5'),
+('AEROPORTI DI ROMA', '6', '6'),
+('Berlin-Tegel airport', '7', '7'),
+('Brussels Airport', '8', '8'),
+('Toronto Pearson International Airport', '9', '9'),
+('Cairo International Airport', '10', '10');
 
 
 CREATE TABLE plane (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `seats` VARCHAR(45) NOT NULL,
-  `type` ENUM('standart', 'military', 'freight') NULL,
-  `airline_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  seats VARCHAR(45) NOT NULL,
+  type ENUM('standart', 'military', 'freight') NULL,
+  airline_id INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_plane_airline1_idx` (`airline_id` ASC) VISIBLE,
   CONSTRAINT `fk_plane_airline1`
@@ -122,31 +122,31 @@ CREATE TABLE plane (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-INSERT INTO plane (id, name, seats, type, airline_id) VALUES
-(1, 'Boeing 777', '100', 'standart', '1'),
-(2, 'Boeing 772', '100', 'military', '2'),
-(3, 'Boeing 737', '100', 'freight', '3'),
-(4, 'Airbus A320', '100', 'military', '4'),
-(5, 'Airbus A330', '100', 'freight', '5'),
-(6, 'Boeing 787', '100', 'standart', '6'),
-(7, 'BOEING 767-300ER', '100', 'standart', '7'),
-(8, 'BOEING 737-800', '100', 'freight', '8'),
-(9, 'EMBRAER-190', '100', 'standart', '9'),
-(10, 'EMBRAER-195', '100', 'military', '10');
+INSERT INTO plane (name, seats, type, airline_id) VALUES
+('Boeing 777', '100', 'standart', '1'),
+('Boeing 772', '100', 'military', '2'),
+('Boeing 737', '100', 'freight', '3'),
+('Airbus A320', '100', 'military', '4'),
+('Airbus A330', '100', 'freight', '5'),
+('Boeing 787', '100', 'standart', '6'),
+('BOEING 767-300ER', '100', 'standart', '7'),
+('BOEING 737-800', '100', 'freight', '8'),
+('EMBRAER-190', '100', 'standart', '9'),
+('EMBRAER-195', '100', 'military', '10');
 
 
 CREATE TABLE flight (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `departure_time` TIMESTAMP(2) NOT NULL,
-  `arrival_time` TIMESTAMP(2) NULL,
-  `allowed_luggage_kg` FLOAT NULL,
-  `connected_flight` INT NULL,
-  `airline_id` INT NOT NULL,
-  `airport_id` INT NOT NULL,
-  `airport_country_id` INT NOT NULL,
-  `airport_id1` INT NOT NULL,
-  `airport_country_id1` INT NOT NULL,
-  `plane_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  departure_time TIMESTAMP(2) NOT NULL,
+  arrival_time TIMESTAMP(2) NULL,
+  allowed_luggage_kg FLOAT NULL,
+  connected_flight INT NULL,
+  airline_id INT NOT NULL,
+  airport_id INT NOT NULL,
+  airport_country_id INT NOT NULL,
+  airport_id1 INT NOT NULL,
+  airport_country_id1 INT NOT NULL,
+  plane_id INT NOT NULL,
   PRIMARY KEY (`id`, `airport_id`, `airport_country_id`, `airport_id1`, `airport_country_id1`, `plane_id`),
   INDEX `fk_flight_airline1_idx` (`airline_id` ASC) VISIBLE,
   INDEX `fk_flight_airport1_idx` (`airport_id` ASC, `airport_country_id` ASC) VISIBLE,
@@ -175,21 +175,21 @@ CREATE TABLE flight (
 
 
 CREATE TABLE user (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `lastname` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(255) NULL,
-  `password` VARCHAR(32) NOT NULL,
-  `passport_id` VARCHAR(45) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  lastname VARCHAR(45) NOT NULL,
+  email VARCHAR(255) NULL,
+  password VARCHAR(32) NOT NULL,
+  passport_id VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`));
 
 
 CREATE TABLE crew (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `main_pilot` VARCHAR(45) NOT NULL,
-  `stewardess` VARCHAR(45) NOT NULL,
-  `second_pilot` VARCHAR(45) NOT NULL,
-  `plane_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  main_pilot VARCHAR(45) NOT NULL,
+  stewardess VARCHAR(45) NOT NULL,
+  second_pilot VARCHAR(45) NOT NULL,
+  plane_id INT NOT NULL,
   PRIMARY KEY (`id`, `plane_id`),
   INDEX `fk_crew_plane1_idx` (`plane_id` ASC) VISIBLE,
   CONSTRAINT `fk_crew_plane1`
@@ -200,11 +200,11 @@ CREATE TABLE crew (
 
 
 CREATE TABLE class (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `type` ENUM('econom', 'premium', 'business') NOT NULL,
-  `benefits` VARCHAR(350) NOT NULL,
-  `price` FLOAT NOT NULL,
-  `flight_id` INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  type ENUM('econom', 'premium', 'business') NOT NULL,
+  benefits VARCHAR(350) NOT NULL,
+  price FLOAT NOT NULL,
+  flight_id INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_class_flight1_idx` (`flight_id` ASC) VISIBLE,
   CONSTRAINT `fk_class_flight1`
@@ -215,13 +215,13 @@ CREATE TABLE class (
 
 
 CREATE TABLE user_has_flight (
-  `user_id` INT NOT NULL,
-  `flight_id` INT NOT NULL,
-  `flight_airport_id` INT NOT NULL,
-  `flight_airport_country_id` INT NOT NULL,
-  `flight_airport_id1` INT NOT NULL,
-  `flight_airport_country_id1` INT NOT NULL,
-  `flight_plane_id` INT NOT NULL,
+  user_id INT NOT NULL,
+  flight_id INT NOT NULL,
+  flight_airport_id INT NOT NULL,
+  flight_airport_country_id INT NOT NULL,
+  flight_airport_id1 INT NOT NULL,
+  flight_airport_country_id1 INT NOT NULL,
+  flight_plane_id INT NOT NULL,
   PRIMARY KEY (`user_id`, `flight_id`, `flight_airport_id`, `flight_airport_country_id`, `flight_airport_id1`, `flight_airport_country_id1`, `flight_plane_id`),
   INDEX `fk_user_has_flight_flight1_idx` (`flight_id` ASC, `flight_airport_id` ASC, `flight_airport_country_id` ASC, `flight_airport_id1` ASC, `flight_airport_country_id1` ASC, `flight_plane_id` ASC) VISIBLE,
   INDEX `fk_user_has_flight_user1_idx` (`user_id` ASC) VISIBLE,
