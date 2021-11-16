1cae721399d14ef8dded3e1233eef9354f50b706
Volodymyr Podufalyy <vpodufalyy13@gmail.com>
2021-11-16 23:08:09 +0200

    Update triggers_2.sql

--- triggers_2.sql
+++ triggers_2.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER  identity_number_format
     on employee
     for each row
 begin
- if (new.identity_number  rlike '.*(?<!00)$') then
+ if (new.identity_number  rlike '.*(?<00)$') then
 		signal sqlstate '45000'
         set message_text = 'identity_number must not end with 2 zeros';
     end if;
